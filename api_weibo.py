95253f4678bc288f2f749553c07b12b41ed2098b
chenyu <chenyulin@colorcode.com.cn>
2017-08-27 16:47:57 +0800

    实现获得关注人列表（第一页）

--- api_weibo.py
+++ api_weibo.py
@@ -1,8 +1,10 @@
 import data_change
+import regular_weibo
+import requests
+import regular_weibo
 
-def body(cookie,url):
-    cookies_str = cookie
-    cookies = str_to_dict(cookies_str)
+def get_body(cookie,url):
+    cookies = data_change.str_to_dict(cookie)
     print('cookies:', cookies)
 
     s = requests.session()
@@ -16,8 +18,17 @@ def body(cookie,url):
     # r.encoding='gbk'
     return  r.text
 
-def get_userId(cookie,url):
-    body = body(cookie,url)
+def get_uid_list(cookie,url='http://weibo.com/p/1005052692248704/myfollow?t=1&cfs=&Pl_Official_RelationMyfollow__111_page=10'):
+    body = get_body(cookie,url)
     regx = regular_weibo.user()
-    return_value = [a.split('\\"')[1] for a in regx.findall(body)]
+    list_1 = [a.split('\\"')[1].rstrip('&') for a in regx.findall(body)]
+    return_value = []
+    for i in list_1:
+        uid = i.split('&')[0].split('=')[1]
+        name = i.split('&')[1].split('=')[1]
+        item = {
+            'uid':uid,
+            'name':name,
+        }
+        return_value.append(item)
     return return_value
--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-import requests
-import regular_weibo
 import api_weibo
-
-def get_userId(cookie):
-    body = api_weibo.body(cookie,'url')
-    regx = regular_weibo.user()
-    return_value = [a.split('\\"')[1] for a in regx.findall(body)]
-    return return_value
+from cookie import cookie
 
 def main():
-    cookie = 'your cookie'
-    items = get_userId(cookie)
+    items = api_weibo.get_uid_list(cookie)
     print(items)
-
+    print(items[0]['uid'])
 main()
