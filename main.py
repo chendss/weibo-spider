aa6bc7c0e87443c125d77bea1c89b74ab8fc1175
chenyu <chenyulin@colorcode.com.cn>
2017-08-27 22:44:40 +0800

    循环遍历获得专辑列表

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import api_weibo
-
+import time
 def main():
     uid_dict = api_weibo.get_uid_list()
-    value = api_weibo.get_photo_urls(uid_dict[2]['uid'])
-    print(uid_dict[2]['name'],uid_dict[2]['uid'])
-    print(value)
+    all_album_list = []
+    for i in uid_dict:
+        time.sleep(1)
+        photos = api_weibo.get_photo_urls(i['uid'])
+        all_album_list.append(photos)
+    print(len(all_album_list))
 main()
